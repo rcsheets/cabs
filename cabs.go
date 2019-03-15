3b539bbc11f2e50c21be39a27bf803fb38117c9c
Robert C. Sheets <rcsheets@acm.org>
2019-03-15 10:12:19 -0700

    Start with some comments

--- cabs.go
+++ cabs.go
@@ -1,5 +1,11 @@
-package cabs
+// Go analogue of npm's content-addressable-blob-store
+//
+// Copyright 2019 Robert Charles Sheets
+//
+// See the LICENSE file for license terms.
 
-func SayHello() string {
-	return "Hello"
-}
+// Package cabs implements a content-addressable blob store with an on-disk
+// format that aims for compatibility with the on-disk format used by
+// https://www.npmjs.com/package/content-addressable-blob-store so that blob
+// stores created with that module can be used by Go projects.
+package cabs
